2f8c7caf3ddc2d717fe216e349a062f1ec223cbc
Katheesh1224 <kgobi24lk@gmail.com>
2025-01-08 10:17:45 +0530

    design completion of chat app

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,13 +1,24 @@
 import './chat.css';
 import React from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo, faPhone, faCircleInfo, faFaceSmile, faPaperPlane, faImage, faCamera, faMicrophone } from '@fortawesome/free-solid-svg-icons';
-
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
+
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState('');
+
+    const handleEmoji = e => {
+        setText(text + e.emoji);
+        setOpen(false);
+    }
+
+
     return(
         <div className='chat'>
-            <div className='partner-info'>
+            <div className='top'>
                 <div className='user'>
                     <img src='./avatar.jpg' alt=''/>
                     <div className='partner'>
@@ -22,17 +33,80 @@ const Chat = () => {
                     <FontAwesomeIcon icon={faCircleInfo} />
                 </div>
             </div>
-            <div className='type-bar'>
+            <div className='middle'>
+                <div className="message">
+                    <p>vwijscbjscbsja cbskc bkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message own">
+                    <p>vwijscbjscbsjacbsk</p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message">
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message own">
+                    <div className="img-box">
+                        <img src='./bmw.jpg' alt=''/>
+                    </div>
+                    <span>09.00 am</span>
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message">
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message own">
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+                <div className="message">
+                    <p>vwijscbjscbsjacbskcbkecksjcnxmckjsbc jskcbsdkcjbscjskcbskckscas
+                        ascnajskcbascascjanscjascbajcbsakjcbsacasc
+                        slcbsckhbashckbascascjbaskc
+                    </p>
+                    <span>09.00 am</span>
+                </div>
+            </div>
+            <div className='bottom'>
                 <div className='icons'>
                     <FontAwesomeIcon icon={faImage} />
                     <FontAwesomeIcon icon={faCamera} />
                     <FontAwesomeIcon icon={faMicrophone} />
                 </div>
-                <div className='message'>
-                    <input type='text' placeholder='Type a message'/>
+                <div className='type'>
+                    <input type='text' 
+                        placeholder='Type a message' 
+                        value={text} 
+                        onChange={(e) => setText(e.target.value)}/>
                 </div>
                 <div className='icons'>
-                    <FontAwesomeIcon icon={faFaceSmile} />
+                    <FontAwesomeIcon icon={faFaceSmile} onClick={() => setOpen((prev) => !prev)}/>
+                    <div className='emoji'>
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
                     <FontAwesomeIcon icon={faPaperPlane} />
                 </div>
             </div>
--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -1,8 +1,108 @@
 import './detail.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp, faExpand } from '@fortawesome/free-solid-svg-icons';
+
 
 const Detail = () => {
     return(
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src='./avatar.jpg' alt=''/>
+                <h2>Vootukari</h2>
+                <p>Pachcha paithiyakaari</p>
+            </div>
+            <div className="detail-items">
+                <div className="item">
+                    <p>Chat Settings</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faAngleDown} size="2xs" />
+                    </div>
+                </div>
+                <div className="item">
+                    <p>Privacy & Help</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faAngleDown} size="2xs" />
+                    </div>                </div>
+                <div className="item">
+                    <p>Shared Photots</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faAngleUp} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item photo">
+                    <img src='./bmw.jpg' alt=''/>
+                    <p>photo_2024_2.jpg</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faExpand} size="2xs" />
+                    </div>                
+                </div>
+                <div className="item">
+                    <p>Shared Files</p>
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faAngleDown} size="2xs" />
+                    </div>                
+                </div>
+                <button>Block User</button>
+            </div>
+            <button className='logout'>Logout</button>
+        </div>
     )
 }
 
